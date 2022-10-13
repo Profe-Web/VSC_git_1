6065c132015d5ffb1113ebf1762c72f1c2687b0e
albert <albertvandellos@tauformar.com>
2022-10-13 10:11:55 +0200

    mCarmen dice la suya

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 // ejemplo
 document.write("Hola TAU!")
 document.write("esto desde la rama branch 'testRama'")
-document.write("---esto desde la rama branch 'testRama2'---")
+document.write("--- MARI CARMEN es la mejor programadora del mundo!---")
